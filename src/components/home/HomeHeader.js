7e0ad4e2cd265389e860007cd0536c125bbd929d
Yano Omurai <52166982+thanhtaimrjoe@users.noreply.github.com>
2022-07-08 10:56:36 +0700

    Design

---
+++ src/components/home/HomeHeader.js
@@ -0,0 +1,71 @@
+import React from "react";
+//style
+import "../../styles/Home.css";
+//ant design
+import { Button, Col, Input, Layout, Menu, Row, Space } from "antd";
+
+const { Header } = Layout;
+const { Search } = Input;
+
+function HomeHeader(props) {
+  //list menu items
+  const menus = [
+    {
+      label: "Sport",
+      key: "item1",
+    },
+    {
+      label: "Romance",
+      key: "item2",
+    },
+    {
+      label: "Action",
+      key: "item3",
+    },
+    {
+      label: "Honor",
+      key: "item4",
+    },
+  ];
+
+  const onSearch = (value) => {
+    console.log("Search", value);
+  };
+
+  return (
+    <Header
+      style={{ padding: "0px", height: "auto", backgroundColor: "white" }}
+    >
+      <Row className="row-menu-title">
+        <Col span={18}>
+          <Space className="space-menu-title">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/yama-98f64.appspot.com/o/materials%2Frestaurant.png?alt=media&token=c67e5e83-a039-4846-9fc2-73143558e270"
+              className="logo"
+              alt="haha"
+            />
+            <Search
+              placeholder="Search anime..."
+              onSearch={onSearch}
+              className="search"
+              size="large"
+              style={{ width: "500px" }}
+            />
+            <Space>
+              <Button>Sign In</Button>
+              <Button>Create new account</Button>
+            </Space>
+          </Space>
+        </Col>
+      </Row>
+      <Menu
+        className="menu"
+        mode="horizontal"
+        defaultChecked={["item1"]}
+        items={menus}
+      />
+    </Header>
+  );
+}
+
+export default HomeHeader;
--- src/screens/home/HomePage.js
+++ src/screens/home/HomePage.js
@@ -2,73 +2,19 @@ import React from "react";
 //style
 import "../../styles/Home.css";
 //ant design
-import { Layout, Menu, Input, Space, Row, Col, Button, Card } from "antd";
+import { Layout, Row, Col, Card } from "antd";
+import HomeHeader from "../../components/home/HomeHeader";
 
-const { Header, Content, Footer } = Layout;
-const { Search } = Input;
+const { Content, Footer } = Layout;
 const { Meta } = Card;
 
 function HomePage(props) {
-  const menus = [
-    {
-      label: "Sport",
-      key: "item1",
-    },
-    {
-      label: "Romance",
-      key: "item2",
-    },
-    {
-      label: "Action",
-      key: "item3",
-    },
-    {
-      label: "Honor",
-      key: "item4",
-    },
-  ];
-
-  const onSearch = (value) => {
-    console.log("Search", value);
-  };
-
   return (
     <Layout>
-      <Header
-        style={{ padding: "0px", height: "auto", backgroundColor: "white" }}
-      >
-        <Row className="row-menu-title">
-          <Col span={18}>
-            <Space className="space-menu-title">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/yama-98f64.appspot.com/o/materials%2Frestaurant.png?alt=media&token=c67e5e83-a039-4846-9fc2-73143558e270"
-                className="logo"
-                alt="haha"
-              />
-              <Search
-                placeholder="Search anime..."
-                onSearch={onSearch}
-                className="search"
-                size="large"
-                style={{ width: "500px" }}
-              />
-              <Space>
-                <Button>Sign In</Button>
-                <Button>Create new account</Button>
-              </Space>
-            </Space>
-          </Col>
-        </Row>
-        <Menu
-          className="menu"
-          mode="horizontal"
-          defaultChecked={["item1"]}
-          items={menus}
-        />
-      </Header>
+      <HomeHeader />
       <Content>
-        <Row className="content-row">
-          <Col span={20}>
+        <Row className="content-row" justify="center">
+          <Col span={20} style={{ backgroundColor: "yellow" }}>
             <Card
               hoverable
               style={{ width: 240 }}
